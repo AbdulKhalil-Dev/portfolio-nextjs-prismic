39f3a8d25976fa3d16a8d58b4cafd53e2757f324
dev-abdul-khalil <khalil.codelab@gmail.com>
2026-06-14 00:39:06 +0500

    biography added

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -4,6 +4,7 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import Button from "@/components/Button";
+import { PrismicNextImage } from "@prismicio/next";
 
 /**
  * Props for `Biography`.
@@ -19,18 +20,26 @@ const Biography: FC<BiographyProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr, 1fr]">
-        <Heading as="h1" size="xl" className="col-start-1">
-          {slice.primary.heading}
-        </Heading>
-        <div className="prose prose-xl prose-slate prose-invert col-start-1">
-          <PrismicRichText field={slice.primary.description} />
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[1fr,2fr] items-start">
+        <div className="md:row-start-1 w-full mx-auto md:max-w-sm rounded-lg shadow-xl shadow-slate-700/50 overflow-hidden bg-slate-900">
+        <PrismicNextImage
+          field={slice.primary.avatar}
+          className="w-full h-auto object-cover"
+        />
+        </div>
+        <div className="md:col-start-2 grid gap-6">
+          <Heading as="h1" size="xl">
+            {slice.primary.heading}
+          </Heading>
+          <div className="prose md:prose-xl prose-slate prose-invert">
+            <PrismicRichText field={slice.primary.description} />
+          </div>
+          <Button
+          linkField={slice.primary.button_link}
+          label={slice.primary.button_text}
+        />
         </div>
       </div>
-      <Button
-        linkField={slice.primary.button_link}
-        label={slice.primary.button_text}
-      />
     </Bounded>
   );
 };
